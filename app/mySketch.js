be6a3fcf2a2e10aa9d26839f188af04669fd4671
arnoldcheung <arnold.cheung1108@gmail.com>
2023-04-26 20:04:39 +0800

    Update mySketch.js

--- app/mySketch.js
+++ app/mySketch.js
@@ -382,7 +382,7 @@ function draw() {
 	mainCanvas.textFont(font);
 	mainCanvas.textAlign(LEFT, TOP);
 	mainCanvas.textSize(50);
-	mainCanvas.text('Test 11', 0, 0);
+	mainCanvas.text('Test 12', 0, 0);
 	mainCanvas.pop();
 
 
