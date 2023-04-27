e945241baeab25917ac7bce9bd00da4d74d5589f
arnoldcheung <arnold.cheung1108@gmail.com>
2023-04-27 18:51:40 +0800

    signature

--- app/mySketch.js
+++ app/mySketch.js
@@ -392,7 +392,7 @@ function draw() {
 	mainCanvas.textFont(font);
 	mainCanvas.textAlign(LEFT, TOP);
 	mainCanvas.textSize(50);
-	mainCanvas.text('Test 29', 0, 0);
+	mainCanvas.text('Test 30', 0, 0);
 	mainCanvas.pop();
 
 
