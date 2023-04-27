ec0dec63dd7677ff6902566e4a496fab0e0da92e
arnoldcheung <arnold.cheung1108@gmail.com>
2023-04-27 20:13:03 +0800

    Update mySketch.js

--- app/mySketch.js
+++ app/mySketch.js
@@ -390,7 +390,7 @@ function draw() {
 	mainCanvas.textFont(font);
 	mainCanvas.textAlign(LEFT, TOP);
 	mainCanvas.textSize(50);
-	mainCanvas.text('Test 30', 0, 0);
+	mainCanvas.text('Test 31', 0, 0);
 	mainCanvas.pop();
 
 	mainCanvas.image(signatureGraphics, 0, 0); // the radiating squares
