c52e336e8be6ab6243c6e162516de2709366e181
Arnold Cheung <arnold.cheung1108@gmail.com>
2023-07-12 14:28:23 +0800

    fix signature

--- app/checkboxHelper.js
+++ app/checkboxHelper.js
@@ -152,7 +152,7 @@ function signatureEvent(){
 	elementName.html(getTranslation('colorNameList')[currentColorSelectionIndex]);
 	elementName.style('color', colorList[currentColorSelectionIndex]);
 
-	signature = false;
+	// signature = false;
 }
 
 function messageEvent(){
